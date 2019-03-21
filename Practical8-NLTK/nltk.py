5e19113d719d3c4df1e603978197f1fd7b27d69b
Mollly <m.asher@hotmail.com>
2019-03-21 13:57:25 +0000

    updates to nltk.py

--- Practical8-NLTK/nltk.py
+++ Practical8-NLTK/nltk.py
@@ -1,27 +1,28 @@
+"""
+Natural language processing of the poem "The Wasteland"
+
+Reads in poem from the Gutenburg Project website.
+Tokenizes the poem into words.
+Computes various statistics on these words and their frequencies.
+Identifies proper nouns through part of speech tagging.
+
+"""
+
 import nltk
-from nltk.tokenize import RegexpTokenizer
+#from nltk.tokenize import RegexpTokenizer
 import requests
-import re
+#import re
 
 # Import extra NLTK packages
 nltk.download("averaged_perceptron_tagger")
 nltk.download("punkt")
 nltk.download("universal_tagset")
 
-"""
-Natural language processing of the poem "The Wasteland"
-
-Reads in poem from the Gutenburg Project website.
-Tokenizes the poem into words and identifies proper nouns through part of speech tagging.
-
-
-"""
-
-# Read in text of "the Wasteland" from website.
+# Read in text of "the Wasteland" from Project Gutenberg website.
 url = "http://www.gutenberg.org/files/1321/1321-0.txt"
 raw = requests.get(url).text
 
-# Trim text to just the poem based on a starting and ending string
+# Trim text to just the poem, based on a starting and ending string
 start_pos = raw.rfind("Nam Sibyllam")
 end_pos = raw.rfind("Line 415 aetherial] aethereal")
 trimmed = raw[start_pos:end_pos]
@@ -29,7 +30,7 @@ trimmed = raw[start_pos:end_pos]
 # Tokenize the raw text
 tokens = nltk.word_tokenize(trimmed)
 
-#  Remove tokens whichare singular punctuation marks
+#  Remove tokens which are singular punctuation marks
 for i in tokens:
     if i.isalnum () == False:
         if len (i) ==1 or len(i) == 2 :
@@ -37,20 +38,22 @@ for i in tokens:
             tokens.remove(i)
 
 
-#convert it into a nltk.Text object
+# Convert tokens into a nltk.Text object
 text = nltk.Text(tokens)
 
-# Find the 20 most common words
+# Find and print the 20 most common words
 fdist_words = nltk.FreqDist(text)
 print(fdist_words.most_common(10))
+# Plot a graph of their frequency
 fdist_words.plot(20)
 
-# Find the 20 most common word lengths
+# Find and print the 20 most common word lengths
 fdist_lengths =  nltk.FreqDist(len(w) for w in text)
 print(fdist_lengths.most_common(20))
 
 # Find all the words over 10 letters long
 long_words = [wrd for wrd in text if len(wrd) > 10]
+print("The following words are over 10 letters long: " + ', '.join(long_words))
 
 # Run part of speech tagging
 #tagged = nltk.pos_tag(text, tagset='universal')
@@ -68,8 +71,8 @@ for tag in tagged:
     if tag[1] == "NNP":
             print (tag)
             proper_nouns.append(tag)
-# Other method
-propernouns2 = [tag for tag,pos in tagged if pos == 'NNP']
+# Alternative method
+#propernouns2 = [tag for tag,pos in tagged if pos == 'NNP']
 
 # Filter out some of the dubious proper nouns
             
