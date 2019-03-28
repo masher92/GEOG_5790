4abd090fd1e6cc3069b4a0b1eca5a03541cadfd8
Molly <m.asher@hotmail.com>
2019-03-28 22:08:16 +0000

    improved documentation to explain process

--- Practical4-GUI/TraffordModelScript.py
+++ Practical4-GUI/TraffordModelScript.py
@@ -1,14 +1,18 @@
 '''
-Script which runs a ModelBuilder Model as a tool within ArcGIS (NB: it does not function as a stand alone script)
-This model constructs a buffer around the locations of a burglaries . 
+Script which runs a ModelBuilder Model as a tool within ArcGIS (NB: it does not function as a stand alone script).
 
-It takes as inputs:
-    The location of burglaries (shp)
-    The distance from a burglary in which the increased risk of burglary is felt
-    The buildings in the area (shp)
+Research suggests that in the 4 weeks following a house getting burgled there is an increased risk of burglary for the houses in the burgled house's immediate vicinity.
+This model uses this theory to analyse the probability of houses in an area getting burgled.  
 
-From this it builds a buffer around the explosion area.
-This is intersected with the buildings to find those which have been destroyed.
+The model takes as inputs:
+* The location of burglaries (shp)
+* The distance from a burglary in which the increased risk of burglary is felt
+* The buildings in the area (shp)
+
+A buffer is built around the locations of the burglaries and then for each building in the area a count is made of how many of
+the burglary buffer zones it intersect with.
+
+This is visualised in....
 
 @author Molly Asher
 @Version 1.0
@@ -19,29 +23,31 @@ import arcpy
 # Set working space.
 arcpy.env.workspace = "E:/Msc/Advanced-Programming/"
 
-# Specify input parameters.
+# Specify input parameters to be used in the model which is run from the toolbox (define the order in which they should be given)
 Burglaries = arcpy.GetParameterAsText(0)
 Distance = arcpy.GetParameterAsText(1)
 Buildings = arcpy.GetParameterAsText(2)
+
+# Specify location for output files to save to (these will be hardwired into the model i.e. the user will not be asked for them)
 crime = "data/generated/Practical4/crime.shp"
 crime_sorted = "data/generated/Practical4/crime_sorted.shp"
 
 # If results already exist at the location specified, then delete them (to avoid overwriting error).
 if arcpy.Exists(crime):
     arcpy.Delete_management (crime)
-
-# Import custom toolbox - "Practical4_Models", rename as models
-arcpy.ImportToolbox("E:/Msc/Advanced-Programming/GitHub/GEOG_5790/Practical4-GUI/Practical4Models.tbx", "models")
-# Run the TraffordModel, from within the models toolbox
-arcpy.TraffordModel_models(Burglaries, Distance, Buildings, crime)
-
-# 
 if arcpy.Exists(crime_sorted):
     arcpy.Delete_management (crime_sorted)
-# Create the output data, sorted. 
+
+# Import custom toolbox - "Practical4_Models", set alias as models
+arcpy.ImportToolbox("E:/Msc/Advanced-Programming/GitHub/GEOG_5790/Practical4-GUI/Practical4Models.tbx", "models")
+
+# Run the TraffordModel from within the models toolbox. 
+arcpy.TraffordModel_models(Burglaries, Distance, Buildings, crime)
+
+# Create the output data sorted in descending order by the number of burglary buffer zones each house is found within. 
 arcpy.Sort_management(crime, crime_sorted, [["Join_Count", "DESCENDING"]])
 
-# Display the results
+# Display the results colour coded by burglary risk. 
 # Get current map document
 mxd = arcpy.mapping.MapDocument("E:/Msc/Advanced-Programming/GitHub/GEOG_5790/Practical4-GUI/Practical4.mxd")
 # Get bit of it currently showing
